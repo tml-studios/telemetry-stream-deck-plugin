93879ce9571d66282b00363dea5e94c9e1ad391f
ByteBitter <bytebitter@gmail.com>
2023-07-21 18:38:01 +0200

    Added Gear Selector
    - Added Gear Selector
    - removed old tooltips

--- src/de.tml-studios.telemetry.sdPlugin/app.js
+++ src/de.tml-studios.telemetry.sdPlugin/app.js
@@ -3,6 +3,7 @@
 
 const DoorAction = new Action('de.tml-studios.telemetry.dooraction');
 const CashAction = new Action('de.tml-studios.telemetry.changeaction');
+const GearSwitchAction = new Action('de.tml-studios.telemetry.gearselect');
 
 var GlobalTargetAddress = null
 var GlobalTargetPort = null
@@ -10,11 +11,13 @@ var LastSendCommand = null
 
 var GlobalInterval = [];
 var GlobalLampData = [];
+var GlobalButtonData = [];
 var GlobalCurrentState = [];
+var GlobalCurrentGear = "";
 
-/**
- * The first event fired when Stream Deck starts
- */
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Global Functions
+
 function SendTelemetryAction(SendTelemetryCommand)
 {
 	LastSendCommand = SendTelemetryCommand
@@ -27,11 +30,12 @@ function SendTelemetryAction(SendTelemetryCommand)
 	{
 		TargetUrl = "http://" + GlobalTargetAddress + ":" + GlobalTargetPort + SendTelemetryCommand
 		fetch(TargetUrl)
+		console.log(TargetUrl)
 		LastSendCommand = null
 	}
 }
 
-function UpdateLightData()
+function UpdateTelemetryData()
 {
 	if(GlobalTargetAddress == null || GlobalTargetPort == null)
 	{
@@ -39,14 +43,84 @@ function UpdateLightData()
 	}
 	else
 	{
-		TargetUrl = "http://" + GlobalTargetAddress + ":" + GlobalTargetPort + "/vehicles/current?vars=AllLamps"
+		TargetUrl = "http://" + GlobalTargetAddress + ":" + GlobalTargetPort + "/vehicles/current?vars=Buttons,AllLamps"
 		fetch(TargetUrl)
 		.then(data => {return data.json()})
 		.then(data => {
-			GlobalLampData = data.AllLamps
+			GlobalLampData = data.AllLamps;
+			GlobalButtonData = data.Buttons;
+			GlobalCurrentGear = GetCurrentGear();
 		})
 	}
 }
+
+$SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) => {
+	console.log('Stream Deck connected!');
+});
+
+
+$SD.onDidReceiveGlobalSettings(({payload}) => 
+{
+	GlobalTargetAddress = payload.settings.TargetIp;
+	GlobalTargetPort = payload.settings.TargetPort;
+
+	if(LastSendCommand != null)
+	{
+		SendTelemetryAction(LastSendCommand);
+	}
+});
+
+function AddInterval(Context, IntervalFunction)
+{
+	if(GlobalInterval["LampDataUpdate"] === undefined)
+	{
+		GlobalInterval["LampDataUpdate"] = setInterval( function() {UpdateTelemetryData() }, 300)
+	}
+
+	if(GlobalInterval[Context] !== undefined)
+	{
+		clearInterval(GlobalInterval[Context]);
+	}
+	GlobalInterval[Context] = setInterval(IntervalFunction, 100)
+}
+
+function RemoveInterval(Context)
+{
+	if(GlobalInterval[Context] !== undefined)
+	{
+		clearInterval(GlobalInterval[Context]);
+	}
+}
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Door Action Functions
+
+DoorAction.onKeyDown(({ action, context, device, event, payload }) => {
+	SendTelemetryAction("/vehicles/current/setbutton?button=" + payload.settings.DoorSelect + "&state=1")
+});
+
+DoorAction.onKeyUp(({ action, context, device, event, payload }) => {
+	SendTelemetryAction("/vehicles/current/setbutton?button=" + payload.settings.DoorSelect + "&state=0")
+});
+
+DoorAction.onWillAppear(({ action, context, device, event, payload }) =>
+{
+	$SD.getSettings(context);
+});
+
+DoorAction.onWillDisappear(({ action, context, device, event, payload }) =>
+{
+	RemoveInterval(context);
+});
+
+$SD.onDidReceiveSettings("de.tml-studios.telemetry.dooraction", ({context, payload}) => 
+{
+	DoorName = payload.settings.DoorSelect
+	if(DoorName === undefined) DoorName = "Front Door";
+
+	AddInterval(context, function() {UpdateButtonLightStatus(payload.settings.DoorSelect + " Light", context)})
+	
+});
 
 function UpdateButtonLightStatus(LightName, context)
 {
@@ -66,49 +140,8 @@ function UpdateButtonLightStatus(LightName, context)
 	}
 }
 
-$SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) => {
-	console.log('Stream Deck connected!');
-});
-
-DoorAction.onKeyDown(({ action, context, device, event, payload }) => {
-	SendTelemetryAction("/vehicles/current/setbutton?button=" + payload.settings.DoorSelect + "&state=1")
-});
-
-DoorAction.onKeyUp(({ action, context, device, event, payload }) => {
-	SendTelemetryAction("/vehicles/current/setbutton?button=" + payload.settings.DoorSelect + "&state=0")
-});
-
-DoorAction.onWillAppear(({ action, context, device, event, payload }) =>
-{
-	$SD.getSettings(context);
-});
-
-DoorAction.onWillDisappear(({ action, context, device, event, payload }) =>
-{
-	if(GlobalInterval[context] !== undefined)
-	{
-		clearInterval(GlobalInterval[context]);
-	}
-});
-
-$SD.onDidReceiveSettings("de.tml-studios.telemetry.dooraction", ({context, payload}) => 
-{
-	//Check LampUpdateInterval
-	if(GlobalInterval["LampDataUpdate"] === undefined)
-	{
-		GlobalInterval["LampDataUpdate"] = setInterval( function() {UpdateLightData() }, 300)
-	}
-
-	if(GlobalInterval[context] !== undefined)
-	{
-		clearInterval(GlobalInterval[context]);
-	}
-	DoorName = payload.settings.DoorSelect
-	if(DoorName === undefined) DoorName = "Front Door";
-	GlobalInterval[context] = setInterval(function() {UpdateButtonLightStatus(payload.settings.DoorSelect + " Light", context)}, 100)
-	
-});
-
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Cash Action Functions
 
 CashAction.onKeyUp(({ action, context, device, event, payload }) => {
 	SendTelemetryAction("/vehicles/current/sendevent?event=" + payload.settings.CashChangeSelect)
@@ -126,67 +159,124 @@ $SD.onDidReceiveSettings("de.tml-studios.telemetry.changeaction", ({context, pay
 	
 
 	if(payload.settings.hasOwnProperty("AutoLabel"))
-	GenerateAutLabel(selected, context)
+		GenerateAutLabel(selected, context)
 });
+
+
+
 
 function GenerateAutLabel(selected, context)
-	{
-		var newTitle = ""
-
-		switch(selected)
-		{
-			case "Coins5":
-				newTitle = "0.05 €";
-				break;
-			case "Coins10":
-				newTitle = "0.10 €";
-				break;
-			case "Coins15":
-				newTitle = "0.15 €";
-				break;
-			case "Coins20":
-				newTitle = "0.20 €";
-				break;
-			case "Coins30":
-				newTitle = "0.30 €";
-				break;
-			case "Coins50":
-				newTitle = "0.50 €";
-				break;
-			case "Coins60":
-				newTitle = "0.60 €";
-				break;
-			case "Coins100":
-				newTitle = "1.00 €";
-				break;
-			case "Coins200":
-				newTitle = "2.00 €";
-				break;
-			case "Coins400":
-				newTitle = "4.00 €";
-				break;
-			case "Coins600":
-				newTitle = "6.00 €";
-				break;
-			case "Coins800":
-				newTitle = "8.00 €";
-				break;
-			case "Take Cash Money":
-				newTitle = "Grab";
-				break;
-		}
-
-		$SD.setTitle(context, newTitle)
-	}
-
-
-$SD.onDidReceiveGlobalSettings(({payload}) => 
 {
-	GlobalTargetAddress = payload.settings.TargetIp;
-	GlobalTargetPort = payload.settings.TargetPort;
+	var newTitle = ""
 
-	if(LastSendCommand != null)
+	switch(selected)
 	{
-		SendTelemetryCommand(LastSendCommand)
+		case "Coins5":
+			newTitle = "0.05 €";
+			break;
+		case "Coins10":
+			newTitle = "0.10 €";
+			break;
+		case "Coins15":
+			newTitle = "0.15 €";
+			break;
+		case "Coins20":
+			newTitle = "0.20 €";
+			break;
+		case "Coins30":
+			newTitle = "0.30 €";
+			break;
+		case "Coins50":
+			newTitle = "0.50 €";
+			break;
+		case "Coins60":
+			newTitle = "0.60 €";
+			break;
+		case "Coins100":
+			newTitle = "1.00 €";
+			break;
+		case "Coins200":
+			newTitle = "2.00 €";
+			break;
+		case "Coins400":
+			newTitle = "4.00 €";
+			break;
+		case "Coins600":
+			newTitle = "6.00 €";
+			break;
+		case "Coins800":
+			newTitle = "8.00 €";
+			break;
+		case "Take Cash Money":
+			newTitle = "Grab";
+			break;
 	}
+
+	$SD.setTitle(context, newTitle)
+}
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Gearswitch Action Functions
+
+function GetCurrentGear()
+{
+	for(button in GlobalButtonData)
+	{
+		if(GlobalButtonData[button].Name == "Gears")
+			return GlobalButtonData[button].State
+	}
+	return null
+}
+
+
+function SetGearswitchIcon(ButtonIndex, context)
+{
+	IconId = "R"
+	ButtonState = "Reverse"
+	if(ButtonIndex == 2)
+	{
+		IconId = "N"
+		ButtonState = "Neutral"
+	}
+	if(ButtonIndex == 1)
+	{
+		IconId = "D";
+		ButtonState = "Drive";
+	}
+
+	IconState = "Normal";
+	if(GlobalCurrentGear == ButtonState)
+		IconState = "Pushed";
+	
+	$SD.setImage(context, "actions/assets/Icon_" + IconId + "_" + IconState)
+}
+
+GearSwitchAction.onKeyUp(({ action, context, device, event, payload }) => {
+	SendTelemetryAction("/vehicles/current/setbutton?button=Gears&state=" + payload.settings.GearSelection)
 });
+
+GearSwitchAction.onWillAppear(({ action, context, device, event, payload }) =>
+{
+	$SD.getSettings(context);
+});
+
+DoorAction.onWillDisappear(({ action, context, device, event, payload }) =>
+{
+	RemoveInterval(context);
+});
+
+$SD.onDidReceiveSettings("de.tml-studios.telemetry.gearselect", ({context, payload}) => 
+{
+	console.log(context);
+	var ButtonGear = payload.settings.GearSelection;
+	if(ButtonGear == undefined)
+		ButtonGear = 2;
+
+	AddInterval(context, function() {SetGearswitchIcon(ButtonGear, context)})
+	
+});
+
+
+
+
